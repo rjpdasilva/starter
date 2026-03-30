e1e3b444ff5caf06aafe6fc48621b04b37d6f451
Ricardo Silva <ricardo.josilva@parceiros.nos.pt>
2026-03-30 15:51:43 +0100

    looks: Add and configure color colorscheme
    Add the `sainnhe/sonokai` colorscheme and set it the default for
    LazyVim.
    
    Signed-off-by: Ricardo Silva <ricardo.josilva@parceiros.nos.pt>

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- Other colorschemes.
+  {
+    "sainnhe/sonokai",
+  },
+  -- Configure LazyVim to use the desired colorscheme.
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+}
